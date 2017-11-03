4ff115e9263c286707c827251c7fb25c1bcdb9cc
Cody <cody@hastings.email>
2017-11-03 18:03:27 -0500

    beginning refactor of home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,33 +4,9 @@ namespace App\Http\Controllers;
 
 class HomeController extends Controller
 {
-    public function showWelcome()
+    public function showHome()
     {
-        return view('welcome');
+        return view('home');
     }
 
-    public function sayHello($name)
-    {
-        $data = array('name' => $name);
-        return view('my-first-view', $data);
-    }
-
-    public function uppercase($word)
-    {
-    $upword = strtoupper($word);
-	$data['upword'] = $upword;
-	return view('uppercase', $data);
-    }
-
-    public function lowercase($word)
-    {
-    $lword = strtolower($word);
-	$data['lword'] = $lword;
-	return view('lowercase', $data);
-    }
-
-    public function increment($number)
-   	{
-   		return $number + 1;
-   	}
 }
