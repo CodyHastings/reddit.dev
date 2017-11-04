82ad1a0bdc0add0961c45effc9fb5472bc618c43
Cody <cody@hastings.email>
2017-11-04 10:57:47 -0500

    updated some info on home with style

--- readme.md
+++ readme.md
@@ -4,8 +4,8 @@ A site to show who Cody Hastings is and what he is capable of.
 
 To do list: 
 
- 1. - [ ] Hide or remove Reddit.dev views. 
- 3. - [ ] Create view for Home and route.
+ 1. - [X] Hide or remove Reddit.dev views. 
+ 3. - [X] Create view for Home and route.
  4. - [ ] Create view for Portfolio and route.
  5. - [ ] Create view for Resume and route.
  6. - [ ] Create view for Blog and route.
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,5 +11,21 @@
 	<main class="container">
 		<div id="header">Cody Hastings</div>
 	</main>
+	<div id="whoIAm">
+		<div class="whoWhatWhy">Who am I?
+			<div class="info">Cody Hastings</div>
+			<div class="info">Full Stack Web Developer</div>
+			<div class="info">Father of 3</div>
+		</div>
+		<div class="whoWhatWhy">What do I do?
+			<div class="info">I solve problems with my brain!</div>
+			<div class="info">I code like a champ!</div>
+			<div class="info">I show up on time and work real hard!</div>
+		</div>
+		<div class="whoWhatWhy">Why do I do it?
+			<div class="info">My family is my world</div>
+			<div class="info">Building awesome stuff rocks!</div>
+		</div>
+	</div>
 
 @stop
